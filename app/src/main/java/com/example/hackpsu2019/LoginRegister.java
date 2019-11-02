699be953da352ced557a184f9774eb653bbbdcd8
WilliamKuDS <williamkucds@gmail.com>
2019-11-02 18:26:56 -0400

    Edit 4: Fixed AndroidManifest, added new pages, add new code to auth

--- app/src/main/java/com/example/hackpsu2019/LoginRegister.java
+++ app/src/main/java/com/example/hackpsu2019/LoginRegister.java
@@ -31,13 +31,6 @@ import java.util.concurrent.TimeUnit;
 
 public class LoginRegister extends AppCompatActivity {
     private String TAG = "Test"; //This is used for the Log.d function which prints out a text on the console.
-    private EditText mPhoneNumber;
-    private EditText mPasswordView;
-    private FirebaseAuth mAuth;
-    private FirebaseAnalytics mFirebaseAnalytics;
-    private String mVerificationId;
-    private PhoneAuthProvider.ForceResendingToken mResendToken;
-    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
 
 
     @Override
@@ -46,18 +39,13 @@ public class LoginRegister extends AppCompatActivity {
         setContentView(R.layout.login_register);
         Log.d(TAG, "onCreate: This is LoginRegister");
 
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        mPhoneNumber = (EditText) findViewById(R.id.login_phone);
         //mPasswordView = (EditText) findViewById(R.id.login_password);
         Button loginButton = (Button)findViewById(R.id.login_button);
-
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String phoneEntered = mPhoneNumber.getText().toString();
-                verifyPhone(phoneEntered, mCallBacks);
+                startActivity(new Intent(LoginRegister.this, Login.class));
 
             }
         });
@@ -91,83 +79,4 @@ public class LoginRegister extends AppCompatActivity {
         }
         return false;
     }
-    public void verifyPhone(String phoneNumber, PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks){
-        PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber,        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,               // Activity (for callback binding)
-                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
-                    @Override
-                    public void onVerificationCompleted(PhoneAuthCredential credential) {
-                        // This callback will be invoked in two situations:
-                        // 1 - Instant verification. In some cases the phone number can be instantly
-                        //     verified without needing to send or enter a verification code.
-                        // 2 - Auto-retrieval. On some devices Google Play services can automatically
-                        //     detect the incoming verification SMS and perform verification without
-                        //     user action.
-                        Log.d(TAG, "onVerificationCompleted:" + credential);
-
-                        signInWithPhoneAuthCredential(credential);
-
-                    }
-
-                    @Override
-                    public void onVerificationFailed(FirebaseException e) {
-                        // This callback is invoked in an invalid request for verification is made,
-                        // for instance if the the phone number format is not valid.
-                        Log.w(TAG, "onVerificationFailed", e);
-
-                        if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                            // Invalid request
-                            // ...
-                        } else if (e instanceof FirebaseTooManyRequestsException) {
-                            // The SMS quota for the project has been exceeded
-                            // ...
-                        }
-
-                        // Show a message and update the UI
-                        // ...
-                    }
-
-                    @Override
-                    public void onCodeSent(@NonNull String verificationId,
-                                           @NonNull PhoneAuthProvider.ForceResendingToken token) {
-                        // The SMS verification code has been sent to the provided phone number, we
-                        // now need to ask the user to enter the code and then construct a credential
-                        // by combining the code with a verification ID.
-                        Log.d(TAG, "onCodeSent:" + verificationId);
-
-                        // Save verification ID and resending token so we can use them later
-                        mVerificationId = verificationId;
-                        mResendToken = token;
-
-                        // ...
-                    }
-                });
-    }
-    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-
-                            FirebaseUser user = task.getResult().getUser();
-                            // ...
-                        } else {
-                            // Sign in failed, display a message and update the UI
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
-                            }
-                        }
-                    }
-                });
-    }
-
-
 }
