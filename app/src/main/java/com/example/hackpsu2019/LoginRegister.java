60a185ea1eeb1d88f0b7e309e52f2d0dddf29e8b
BuildTools <unconfigured@null.spigotmc.org>
2019-11-02 15:52:10 -0400

    First Edit: Added start of Firebase and Mapping. Also included William's Login Screen

--- app/src/main/java/com/example/hackpsu2019/LoginRegister.java
+++ app/src/main/java/com/example/hackpsu2019/LoginRegister.java
@@ -1,21 +1,53 @@
 package com.example.hackpsu2019;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginRegister extends AppCompatActivity {
     private String TAG = "Test"; //This is used for the Log.d function which prints out a text on the console.
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private FirebaseAuth mAuth;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_register);
         Log.d(TAG, "onCreate: This is LoginRegister");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.login_phone);
+        //mPasswordView = (EditText) findViewById(R.id.login_password);
+
+        Button loginButton = (Button)findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginRegister.this, MainPage.class));
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/hackpsu2019/MainPage.java
+++ app/src/main/java/com/example/hackpsu2019/MainPage.java
@@ -3,14 +3,82 @@ package com.example.hackpsu2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import androidx.annotation.NonNull;
+
 
 public class MainPage extends AppCompatActivity {
-    private String TAG = "Test";
+
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1Ijoid2lsbGlhbWt1OTciLCJhIjoiY2syaHV4NGxwMGRxZDNobnRndHQ0bTd4bSJ9.rtPcDlPrnOPg8BcfsyEhDw");
         setContentView(R.layout.main_page);
-        Log.d(TAG, "onCreate: This is MainPage");
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+
+                    }
+                });
+            }
+        });
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
 }
